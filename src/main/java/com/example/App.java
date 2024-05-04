db9516750d41d388cfecc16e6258a0fbc0fff264
kush kumar <kush98631@gmail.com>
2024-05-04 20:02:04 +0530

    modify app file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! dfghjfcgvhbjnk" );
+
+int a=10;
+int b=20;
+int c=int a+b;
+System.out.println(c);
     }
 }
